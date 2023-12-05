34cb22430e9fda4f28bd18030558bb950e8f938f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2023-12-05 00:34:40 +0000

    Section 02: Styling React Native apps

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text!</Text>
+        <Text style={styles.text}>Another piece of text!</Text>
       </View>
-      <Text>Hello World!</Text>
+      <Text style={styles.text}>
+        Hello World!
+      </Text>
       <Button title='Tap me!' />
     </View>
   );
@@ -18,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    margin: 16, 
+    borderWidth: 1, 
+    borderColor: 'red', 
+    padding: 16,
   },
 });
